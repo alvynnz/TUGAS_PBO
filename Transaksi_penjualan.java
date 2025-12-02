e5350a31b91dc9dc57b67864e4ed66c740dbe399
nadiasalwa <salwanadia416@gmail.com>
2025-12-02 23:00:48 +0700

    detail n transaksi

---
+++ Transaksi_penjualan.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Transaksi_penjualan {
+    private String kodeTransaksi;
+    private Date tanggal;
+    private String kasir;
+    private ArrayList<Detail_transaksi> listDetail;
+
+    public Transaksi_penjualan(String kodeTransaksi, String kasir) {
+        this.kodeTransaksi = kodeTransaksi;
+        this.kasir = kasir;
+        this.tanggal = new Date();
+        this.listDetail = new ArrayList<>();
+    }
+
+    public void tambahDetail(Detail_transaksi detail) {
+        listDetail.add(detail);
+    }
+
+    public double hitungTotal() {
+        double total = 0;
+        for (Detail_transaksi d : listDetail) {
+            total += d.hitungSubtotal();
+        }
+        return total;
+    }
+
+    public void tampilkanTransaksi() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        System.out.println("===== TRANSAKSI PENJUALAN =====");
+        System.out.println("Kode : " + kodeTransaksi);
+        System.out.println("Tanggal : " + sdf.format(tanggal));
+        System.out.println("Kasir : " + kasir);
+        System.out.println("\n-- DETAIL --");
+        
+        for (Detail_transaksi d : listDetail) {
+            System.out.println(d.toString());
+        }
+
+        System.out.println("\nTOTAL BAYAR : " + hitungTotal());
+    }
+}
+
